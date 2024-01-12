99d9c1b94265d95bbbbb2cc26588a353f81b9fa7
“jongwon3263” <jongwon3263@gmail.com>
2024-01-12 12:16:30 +0900

    qwfsd

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ indexNumber = st.sidebar.text_input("number")
 
 tab1, tab2 = st.tabs(["메시지","일정"])
 
-conn = st.connection("gsheets", type=GSheetsConnection, ttl="10m")
+conn = st.connection("gsheets", type=GSheetsConnection)
 
 data = conn.read(worksheet="everyhome2024")
 df = pd.DataFrame(data)

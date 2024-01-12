e9664f64e354f2a4f2d84fcf333dcef4e59c0301
“jongwon3263” <jongwon3263@gmail.com>
2024-01-12 11:52:31 +0900

    adf

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ indexNumber = st.sidebar.text_input("number")
 tab1, tab2 = st.tabs(["메시지","일정"])
 
 conn = st.connection("gsheets", type=GSheetsConnection)
-data = conn.read(worksheet = "everyhome2024")
+data = conn.read(worksheet="everyhome2024")
 df = pd.DataFrame(data)
 
 rowSelected = df.loc[(df.phone == phoneNumber) | [indexNumber]]
